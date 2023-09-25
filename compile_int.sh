d325011e230badf39522a5cfb148060b5949a416
nscapin <nicolos@kth.se>
2023-09-24 23:20:03 -0400

    add the intermediate file

---
+++ compile_int.sh
@@ -0,0 +1,6 @@
+#
+mkdir -p windwave_turb
+#
+# compile
+#
+CC99='mpicc -std=c99' qcc -grid=octree -events -autolink -O2 -g -Wall -pipe -D_FORTIFY_SOURCE=2 -D_MPI=1 -DTREE -o windwave_turb/windwave_turb curved_fixREtau_int.c -L$BASILISK/gl -lfb_tiny -lm
