8be6d64480c4a8160c07d82aa3c30e8938532dff
Nicolo' Scapin <ns8802@tiger3.princeton.edu>
2024-11-23 00:56:53 -0500

    update

---
+++ sbatch_files/run_tiger3.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --job-name=720_0p3          # create a short name for your job
+#SBATCH --nodes=1                   # node count
+#SBATCH --ntasks-per-node=112       # number of tasks per node
+#SBATCH --cpus-per-task=1           # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G            # memory per cpu-core (4G is default)
+##SBATCH --time=23:59:00             # total run time limit (HH:MM:SS)
+#SBATCH --time=00:30:00             # total run time limit (HH:MM:SS)
+#SBATCH --account=cimes             # total run time limit (HH:MM:SS)
+####SBATCH --mail-type=begin        # send email when job begins
+####SBATCH --mail-type=end          # send email when job ends
+####SBATCH --mail-user=ns8802@princeton.edu
+#
+module load intel-tbb/2021.13 intel-rt/2024.2
+module load intel-oneapi/2024.2
+module load intel-mpi/oneapi/2021.13
+#
+module load intel-mpi/gcc/2021.3.1
+#
+ulimit -s unlimited
+#
+# Input parameters
+#
+Re_ast=720.0;
+BO=1000.0;
+Re_wave=2.5597571943e+04;
+UstarRATIO=0.9;
+ak=0.3;
+r_L0lam=4.0;
+rho_r=0.001225;           
+mu_r=2.2471881948940954e-06; 
+MAXLEVEL=10;
+MINLEVEL=6;
+RELEASETIME=3609.255;
+uemaxRATIO=0.3;
+end_sim=1000000.0;
+do_eta_loc=1;
+do_profile=1;
+do_fields=1;
+do_tagging=0;
+prt_res=9; 
+from_pr=1;
+st_wave=0;
+rand_num=2;
+#
+srun windwave_turb -m 23:59:00 $Re_ast $BO $Re_wave $UstarRATIO $ak $r_L0lam \
+	                       $rho_r $mu_r $MAXLEVEL $MINLEVEL $RELEASETIME \
+	                       $uemaxRATIO $end_sim \
+			       $do_eta_loc $do_profile $do_fields $do_tagging \
+	                       $prt_res $from_pr \
+	                       $st_wave $rand_num > out.log 2>&1
+#
